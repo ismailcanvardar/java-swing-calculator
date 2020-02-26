40d296a8ad12152f9e872a2d009c0cf660cbc6f7
Ismail Can Vardar <ismailcanvardar@gmail.com>
2020-02-26 20:15:42 +0300

    Converter methods has been altered.

--- src/com/mythi/calculator/Converter.java
+++ src/com/mythi/calculator/Converter.java
@@ -1,4 +1,16 @@
 package com.mythi.calculator;
 
-public interface Converter {
+public abstract class Converter {
+    public static int[] convertToInt(String number1, String number2) {
+        int a = Integer.parseInt(number1);
+        int b = Integer.parseInt(number2);
+
+        return new int[]{ a, b };
+    }
+
+    public String convertToString(int number1) {
+        String convertedInteger = String.valueOf(number1);
+
+        return convertedInteger;
+    }
 }
